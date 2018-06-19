95d21d52f8d6cefdfb89632db580c6bae70aaa5d
晃弘 上山 <akihiro@hololab.co.jp>
2018-06-20 02:44:53 +0900

    first commit

---
+++ WebSocketControl/Assets/WebSocketControl/Scripts/WebSocketClientManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WebSocketConteol
+{
+    public class WebSocketClientManager
+    {
+        public WebSocketClientManager()
+        {
+
+        }
+    }
+}
